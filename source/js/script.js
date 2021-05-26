d27d16e418671bba73b324334e2390cee038ee14
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-05-26 23:46:09 +0300

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,9 @@
 
 let menu = document.querySelector(".main-nav");
 let openMenu = document.querySelector(".toggle");
+let form = document.querySelector(".form");
+let inputTel = document.querySelectorAll("#tel");
+let inputName = document.querySelectorAll("#name");
 
 openMenu.classList.add("toggle--closed");
 menu.classList.remove("main-nav--open");
@@ -11,3 +14,13 @@ openMenu.addEventListener("click", function () {
   openMenu.classList.toggle("toggle--closed");
   openMenu.classList.toggle("toggle--opened");
 });
+
+
+// form.addEventListener("submit", function (evt) {
+//   if (!inputTel.value || !inputName.value) {
+//     evt.preventDefault();
+//   } else {
+//     localStorage.setItem("tel", inputTel.value);
+//     localStorage.setItem("email", inputName.value);
+//   }
+// });
