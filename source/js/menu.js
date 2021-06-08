6fcd1258bf027bc08ab83b5de97b272f2ade0330
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-06-08 22:10:56 +0300

    fixes desktop scroll and header

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ const menu = document.querySelector(".main-nav");
 const menuButton = document.querySelector(".toggle");
 const body = document.querySelector(".page__body");
 const links = document.querySelectorAll(".main-nav__link");
+const header = document.querySelector(".header__wrapper");
 
 const closedMenu = () => {
   menuButton.classList.add("toggle--closed");
@@ -17,11 +18,14 @@ menuButton.addEventListener("click", () => {
     menu.classList.add("main-nav--open");
     menuButton.classList.remove("toggle--closed");
     menuButton.classList.add("toggle--opened");
+    menu.classList.add("main-nav--open");
+    header.classList.add("header__wrapper--menu-opened");
   } else if (menuButton.classList.contains("toggle--opened")) { // меню закрыто
       body.classList.remove("page__body--lock");
       menu.classList.remove("main-nav--open");
       menuButton.classList.remove("toggle--opened");
       menuButton.classList.add("toggle--closed");
+      header.classList.remove("header__wrapper--menu-opened");
   }
 });
 
