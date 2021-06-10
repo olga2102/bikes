c53fdf75f867fc6fe1cd539cab5b0f0170ddaf7e
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-06-10 21:23:30 +0300

    fixed ghpages

--- deploy.js
+++ deploy.js
@@ -1,8 +1,4 @@
 const ghPages = require('gh-pages');
-const gulp = require("gulp");
-// ghPages.publish("build", function(err) {});
 
-gulp.task('deploy', function() {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
-});
+ghPages.publish("build", function(err) {});
+
